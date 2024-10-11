53fdf8f649181f191185240df2870494e25e3b17
cercatr0va <rocknblock23@gmail.com>
2024-10-11 23:57:23 +0800

    add all const

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 pygame.display.set_caption('Shooting Game')
-icon = pygame.image.load("img/")
+icon = pygame.image.load("img/1.jpg")
 pygame.display.set_icon(icon)
 
-target_img = pygame.image.load("img/")
+target_img = pygame.image.load("img/a.png")
 target_width = 80
 target_height = 80
 
